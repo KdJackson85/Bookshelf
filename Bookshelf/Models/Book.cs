21dc269610371c2cac636eb1f5ae06fb54dbca7b
Keith Jackson <kj5thguitar@gmail.com>
2017-02-06 13:40:24 -0500

    created category.cs

---
+++ Bookshelf/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web;
+
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public int Description { get; set; }
+        public string ISBN { get; set; }
+
+
+        [ForeignKey("Catagory")]
+        public int CatagoryID { get; set; }
+        public virtual Catagory Catagory { get; set; }
+    }
+}
---
+++ Bookshelf/Models/Catagory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf.Models
+{
+    public class Catagory
+    {
+        
+        
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public virtual ICollection<Book> Books { get; set; }
+        
+    }
+}
